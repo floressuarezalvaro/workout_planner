dfb232310752c01bb6564bd39fc67c79505c4370
Alvy <floressuarezalvaro@gmail.com>
2021-02-07 20:58:03 -0800

    added api workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,13 @@ const Workout = require("../models/workouts");
 
 router.get("/api/workouts", (req, res) => {
   console.log("hello workouts");
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
